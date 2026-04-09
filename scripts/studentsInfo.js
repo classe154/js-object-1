751e402f60361a819b25213cc974491fa07f9a71
Samuel Panicucci <samuel.panicucci@gmail.com>
2026-04-09 09:07:23 +0200

    first commit

---
+++ scripts/studentsInfo.js
@@ -0,0 +1,177 @@
+const studentsInfo = [
+  {
+    "id": 8887,
+    "name": "Federico",
+    "lastname": "Acker",
+    "github_user": "federicoacker",
+    "image": "https://avatars.githubusercontent.com/u/509528?v=4"
+  },
+  {
+    "id": 8888,
+    "name": "Carlo",
+    "lastname": "Almanza",
+    "github_user": "CarloAlmanza",
+    "image": "https://avatars.githubusercontent.com/u/263946460?v=4"
+  },
+  {
+    "id": 8889,
+    "name": "Nilde",
+    "lastname": "Benedetti",
+    "github_user": "nildebenedetti",
+    "image": "https://avatars.githubusercontent.com/u/263934205?v=4"
+  },
+  {
+    "id": 8890,
+    "name": "Giulia",
+    "lastname": "Buonasperanza",
+    "github_user": "giulia-buonasperanza",
+    "image": "https://avatars.githubusercontent.com/u/263946222?v=4"
+  },
+  {
+    "id": 8891,
+    "name": "Ivan Vincenzo",
+    "lastname": "Caldarella",
+    "github_user": "gamein3x",
+    "image": "https://avatars.githubusercontent.com/u/255232199?v=4"
+  },
+  {
+    "id": 8892,
+    "name": "Filippo",
+    "lastname": "Casadei ",
+    "github_user": "filippo-casadei",
+    "image": "https://avatars.githubusercontent.com/u/263928541?v=4"
+  },
+  {
+    "id": 8893,
+    "name": "Francesco",
+    "lastname": "Cassese",
+    "github_user": "francesco-cassese",
+    "image": "https://avatars.githubusercontent.com/u/263924667?v=4"
+  },
+  {
+    "id": 8894,
+    "name": "Raffaele",
+    "lastname": "De Pasca",
+    "github_user": "depasca-raffaele",
+    "image": "https://avatars.githubusercontent.com/u/263920464?v=4"
+  },
+  {
+    "id": 8895,
+    "name": "Alessia",
+    "lastname": "Di Ruggero",
+    "github_user": "rAel-Ael",
+    "image": "https://avatars.githubusercontent.com/u/209122594?v=4"
+  },
+  {
+    "id": 8896,
+    "name": "Abdeslam",
+    "lastname": "El Ftouh",
+    "github_user": "AbdeslamElFtouh",
+    "image": "https://avatars.githubusercontent.com/u/263944986?v=4"
+  },
+  {
+    "id": 8897,
+    "name": "Lorenzo",
+    "lastname": "Favale",
+    "github_user": "fralofa15",
+    "image": "https://avatars.githubusercontent.com/u/208270196?v=4"
+  },
+  {
+    "id": 8898,
+    "name": "Marco",
+    "lastname": "Fiordi",
+    "github_user": "MarcoFiordi",
+    "image": "https://avatars.githubusercontent.com/u/263918418?v=4"
+  },
+  {
+    "id": 8899,
+    "name": "Francesco Emanuele",
+    "lastname": "Floris",
+    "github_user": "FrancescoEFloris",
+    "image": "https://avatars.githubusercontent.com/u/263896087?v=4"
+  },
+  {
+    "id": 8900,
+    "name": "Giovanni Paolo",
+    "lastname": "Ghinet",
+    "github_user": "Giovanni-Ghinet",
+    "image": "https://avatars.githubusercontent.com/u/263895995?v=4"
+  },
+  {
+    "id": 8901,
+    "name": "Bin Giulio",
+    "lastname": "Liu",
+    "github_user": "bingiulioliu",
+    "image": "https://avatars.githubusercontent.com/u/171387714?v=4"
+  },
+  {
+    "id": 8902,
+    "name": "Sara",
+    "lastname": "Luongo ",
+    "github_user": "Sara-Luongo",
+    "image": "https://avatars.githubusercontent.com/u/263934500?v=4"
+  },
+  {
+    "id": 8903,
+    "name": "Chiara",
+    "lastname": "Oriolo",
+    "github_user": "ChiaraOriolo",
+    "image": "https://avatars.githubusercontent.com/u/263428831?v=4"
+  },
+  {
+    "id": 8904,
+    "name": "Maurizio",
+    "lastname": "Palmisano",
+    "github_user": "mauriziopalmisano",
+    "image": "https://avatars.githubusercontent.com/u/263934182?v=4"
+  },
+  {
+    "id": 8905,
+    "name": "Simone",
+    "lastname": "Palmisciano",
+    "github_user": "SimonePalmisciano",
+    "image": "https://avatars.githubusercontent.com/u/264183760?v=4"
+  },
+  {
+    "id": 8906,
+    "name": "Amirali",
+    "lastname": "Rezaei",
+    "github_user": "amirali-rezaei92",
+    "image": "https://avatars.githubusercontent.com/u/210031722?v=4"
+  },
+  {
+    "id": 8907,
+    "name": "Aldo",
+    "lastname": "Sibilio",
+    "github_user": "Aldo-Sibilio",
+    "image": "https://avatars.githubusercontent.com/u/244370673?v=4"
+  },
+  {
+    "id": 8908,
+    "name": "Sara",
+    "lastname": "Sidhom",
+    "github_user": "Sarasidhom29",
+    "image": "https://avatars.githubusercontent.com/u/263946336?v=4"
+  },
+  {
+    "id": 8909,
+    "name": "Alessia ",
+    "lastname": "Smeraglia ",
+    "github_user": "alessiaasmeraglia",
+    "image": "https://avatars.githubusercontent.com/u/63261245?v=4"
+  },
+  {
+    "id": 8910,
+    "name": "Alessandro",
+    "lastname": "Tiberia",
+    "github_user": "alessandrotiberia",
+    "image": "https://avatars.githubusercontent.com/u/150442623?v=4"
+  },
+  {
+    "id": 8911,
+    "name": "Giorgio",
+    "lastname": "Torricelli",
+    "github_user": "giorgiotorricelli",
+    "image": "https://avatars.githubusercontent.com/u/263920610?v=4"
+  }
+]
